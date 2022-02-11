d722e8ca47cedd81f3e5447aec568529ad2ceb87
Gorohov <d.gorohov@itransition.com>
2022-02-11 15:45:17 +0300

    corrected search value

--- client/pages/search/index.tsx
+++ client/pages/search/index.tsx
@@ -12,10 +12,10 @@ import useDebounce from "../../utils/useDebounce";
 
 const Search: NextPage = () => {
   const [value, setValue] = useState("");
-  const debounce = useDebounce(value, 1000);
+  const searchValue = useDebounce(value, 1000);
   const { data, fetchMore } = useQuery<IMovieSearch>(SEARCH_MOVIES, {
     variables: {
-      title: debounce,
+      title: searchValue,
     },
   });
   const movies = data?.searchMovies?.results;
